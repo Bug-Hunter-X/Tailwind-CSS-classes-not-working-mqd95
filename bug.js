0d27a0ff1ac2e35e978f751e0f74fb90133d9c5d
BugHunter-X <askzenai@gmail.com>
2025-02-07 17:42:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+//MyComponent.jsx
+<div className="bg-gray-200 p-4">
+  <h2 className="text-xl font-bold">My Component</h2>
+  <p>This is some text.</p>
+</div>
+```
+This code snippet uses Tailwind CSS classes to style a div element. However, the `bg-gray-200` class is not working as expected, and the background color is not changing.
